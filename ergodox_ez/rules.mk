255787b04e592e8e27aa54b2c67c06db7c736cd0
Phong Nguyen <nhphong1406@gmail.com>
2020-07-10 20:06:33 +0700

    [ERGODOX-EZ] Disable RGBLIGHT_ENABLE

--- ergodox_ez/rules.mk
+++ ergodox_ez/rules.mk
@@ -3,3 +3,4 @@ EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
 COMMAND_ENABLE   = no  # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE      = yes # Nkey Rollover
+RGBLIGHT_ENABLE  = no
