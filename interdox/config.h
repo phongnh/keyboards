0caf387aa631a3ada7639d976e883218bc120663
Phong Nguyen <nhphong1406@gmail.com>
2020-07-13 10:30:53 +0700

    [INTERDOX] Update layers
    Add README
    
    Fix some keys
    
    Update README
    
    Add EPRM key
    
    Add AG_NORM and AG_SWAP keys to _MOUSE layer
    
    ADD arrow keys
    
    Add _EDGE and _SYMBOL layers
    
    Set keyboard info for Interdox
    
    Update layers
    
    Revert previous change
    
    Fix led
    
    Update layers and readme

--- interdox/config.h
+++ interdox/config.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#undef  MANUFACTURER
+#define MANUFACTURER Interdox Wireless
+#undef  PRODUCT
+#define PRODUCT      Interdox_Wireless
+#undef  DESCRIPTION
+#define DESCRIPTION  q.m.k. keyboard firmware for Interdox Wireless
+
 // Copy from ErgoDox EZ
 #define MOUSEKEY_INTERVAL    20
 #define MOUSEKEY_DELAY       0
@@ -10,3 +17,9 @@
 #define TAPPING_TERM    200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
+// Always send Escape if Alt is pressed
+#define GRAVE_ESC_ALT_OVERRIDE
+// Always send Escape if Control is pressed
+#define GRAVE_ESC_CTRL_OVERRIDE
+// Always send Escape if GUI is pressed
+#define GRAVE_ESC_GUI_OVERRIDE
